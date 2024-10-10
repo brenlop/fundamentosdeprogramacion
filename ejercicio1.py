92e48d669c6dbf7b45c7788bf7deca4ab330adf3
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-10-10 10:00:02 -0600

    01 primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+"""
+ ejercicio 1:
+ escribir un programa que pregunte al usuario su nombre
+  e imprimir un saludo 
+entradas:
+nombre:string
+salida:
+saludo:strring
+"""
+nombre = input("escribe tu nombre:")
+saludo ="holaa "+nombre+"!"
+print(saludo)
