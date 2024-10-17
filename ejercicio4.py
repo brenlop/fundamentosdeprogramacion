ed8c57f1a3a6f072129270bc4aabddc765967a8d
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-10-17 09:44:06 -0600

    ejercicio.4

---
+++ ejercicio4.py
@@ -0,0 +1,25 @@
+"""
+ejercicio 4:
+programa que calcular dos numeros , mostrar suma ,reststa 
+multiplicacion de ambos 
+entrada:
+num1:int
+num2:int
+salida
+resta
+multiplicacion 
+suma 
+division
+"""
+valor1= input("ingresar primer numero:")
+valor1=int(valor1)
+valor2= input("ingresar segundo numero")
+valor2=int(valor2)
+suma = valor1 + valor2
+resta = valor1 - valor2
+multiplicacion = valor1 * valor2
+division = valor1 / valor2
+print("la suma de los dos valores es:",suma)
+print("la resta de los dos valores es:",resta)
+print("la multiplicacion de los dos valores es:",multiplicacion)
+print("la divicion de los dos valores es:",division)
