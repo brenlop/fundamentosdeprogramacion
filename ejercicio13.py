8c3045082761b3b9900e34bfca5d50adf0c8d71c
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-10-20 20:25:19 -0600

    ejercicio13

---
+++ ejercicio13.py
@@ -0,0 +1,16 @@
+"""
+ejercicio13:
+realizar un algoritmo que lea un numero y que muestre su raiz cuadrada y su
+raiz cubica
+entrada:
+raiz cuadrada
+raiz cubica 
+salida :
+calcular la raiz
+"""
+raiz _ cuadrada = numero ** 0.5
+raiz _ cubica = numero **(1/3)
+print("la raiz cuadada es:")
+int(raiz_cuadrada))
+print("la raiz_cubica es:",)
+int(raiz_cubica)
---
+++ ejercicio14.py
@@ -0,0 +1,12 @@
+"""
+ejercicio14:
+un numero de dos cifrass ,diseñe un algoritmo que permite obtener el 
+numero invertido
+entrada:
+numeros 
+salida:
+numero invertido
+"""
+num = input("ingresa un numero de dos cifras:")
+num = int(num)
+dec = 
